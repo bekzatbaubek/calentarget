3f35803b862b1e0f54291bd6f14636012c5becb4
Placathose <61745986+Placathose@users.noreply.github.com>
2021-10-29 17:39:44 +0800

    text input trial

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+      style={styles.input}/>
+      <Text>Hello, Bruce</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +21,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 300
+  }
 });
