e447aa95df1d5f61e128e9b506046aefabcc6bd9
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2021-11-24 09:36:00 +0800

    Timer (without circle)

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet } from 'react-native';
 
 export default function Pomodoro() {
+
+  const [secondsLeft, setSecondsLeft] = useState(25 * 60);
+  const [timer, setTimer] = useState();
+
+  const startTimer = () => {
+    const timer = setInterval(() => {
+      setSecondsLeft((secondsLeft) => secondsLeft - 1);
+      if (secondsLeft === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+    setTimer(timer);
+  };
+
+  useEffect(() => {
+    if (secondsLeft === 0) {
+      clearInterval(timer);
+    }
+  }, [secondsLeft, timer]);
+
+  const formatTime = (seconds) => {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+    let minuteString = minutes > 10 ? minutes : '0' + minutes;
+    let secondsString = remainingSeconds > 10 ? remainingSeconds : '0' + remainingSeconds;
+    return minuteString + ':' + secondsString;
+  }
+
+  useEffect(() => {
+    return () => clearInterval(timer);
+  }, [timer]);
+
   return (
     <SafeAreaView  style={ styles.position } >
       <Text style={ styles.banner }>Pomodoro Screen</Text>
+      <Text>{formatTime(secondsLeft)}</Text>
+      <Button onPress={startTimer} title="Start"/>
     </SafeAreaView >
   );
 }
@@ -14,7 +48,6 @@ export default function Pomodoro() {
 const styles = StyleSheet.create({
   banner: {
     color: 'red'
-    
   },
 
   position: {
@@ -22,5 +55,4 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     justifyContent: 'center',
   }
-  
 })
