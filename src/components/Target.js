3c859e356357e548f87c83301ff8bf3b446977e8
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2021-12-01 13:08:20 +0800

    Display targets title+number of targets as emoji

--- src/components/Target.js
+++ src/components/Target.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, Text, Button, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+const singleTarget = (target) => {
+  return <View>
+    <Text>{target.title}</Text>
+    <Text>{"🎓".repeat(target.numberOfTargets)}</Text>
+  </View>
+}
+
+const stubTargets = [
+  {
+    title: "University",
+    numberOfTargets: 3
+  },
+  {
+    title: "Personal",
+    numberOfTargets: 4
+  },
+  {
+    title: "Whatever",
+    numberOfTargets: 5
+  },
+]
+
 export default function Target() {
+
+  const [targets, setTargets] = useState(stubTargets);
+
   return (
     <SafeAreaView  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Target Screen</Text>
+      <FlatList
+        data={targets}
+        renderItem={({item}) =>
+          singleTarget(item)
+        }
+        keyExtractor={(item, index) => index}
+      />
+      <TextInput style={{ borderWidth: 1, width: 100 }}/>
+      <Button title="Add a target"/>
     </SafeAreaView >
   );
 }
