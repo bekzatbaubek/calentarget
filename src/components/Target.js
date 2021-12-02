33e52dfa17e9e5e17ce5fbd3477e88fa52830e38
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2021-12-02 16:05:45 +0800

    add targets to db skeleton

--- src/components/Target.js
+++ src/components/Target.js
@@ -2,6 +2,25 @@ import React, { useState } from 'react';
 import { FlatList, Text, Button, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import * as SQLite from "expo-sqlite";
+
+function openDatabase() {
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+
+  const db = SQLite.openDatabase("db.db");
+  return db;
+}
+
+const db = openDatabase();
+
 const singleTarget = (target) => {
   return <View>
     <Text>{target.title}</Text>
@@ -26,7 +45,51 @@ const stubTargets = [
 
 export default function Target() {
 
+  React.useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists targets (id integer primary key not null, title text, pending int, completed int);"
+      );
+    });
+  }, []);
+
   const [targets, setTargets] = useState(stubTargets);
+  const [newTargetText, setNewTargetText] = useState("");
+  const [newTargetNumber, setNewTargetNumber] = useState(1);
+  const [forceUpdate, forceUpdateId] = useForceUpdate();
+
+  const addNewTargetToDB = (newTargetText) => {
+    if (newTargetText === "" || newTargetText === null) {
+      return false;
+    }
+    console.log(`Adding ${newTargetText} to the database`);
+
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into targets (title, pending, completed) values (?, 1, 0)", [newTargetText]);
+        tx.executeSql("select * from targets", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      },
+      null,
+      forceUpdate
+    );
+  }
+
+  const dropTable = () => {
+    console.log("Dropping db...");
+    db.transaction(
+      (tx) => {
+        tx.executeSql("drop table targets", []);
+        console.log("DB table drop successful");
+        tx.executeSql(
+          "create table if not exists targets (id integer primary key not null, title text, pending int, completed int);"
+        );
+      },
+      null,
+      forceUpdate
+    );
+  }
 
   return (
     <SafeAreaView  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -37,8 +100,20 @@ export default function Target() {
         }
         keyExtractor={(item, index) => index}
       />
-      <TextInput style={{ borderWidth: 1, width: 100 }}/>
-      <Button title="Add a target"/>
+      <TextInput
+        onChangeText={(text) => setNewTargetText(text)}
+        style={{ borderWidth: 1, width: 100 }}/>
+      <Button
+        onPress={() => addNewTargetToDB(newTargetText)}
+        title="Add a target"/>
+      <Button
+        onPress={() => dropTable()}
+        title="Clean DB"/>
     </SafeAreaView >
   );
+}
+
+function useForceUpdate() {
+  const [value, setValue] = useState(0);
+  return [() => setValue(value + 1), value];
 }
